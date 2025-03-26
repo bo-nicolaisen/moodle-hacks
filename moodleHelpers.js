d815a914bf87e2156525482454af09c07a466918
Bo Nicolajsen <>
2025-03-26 09:14:55 +0100

    https

--- moodleHelpers.js
+++ moodleHelpers.js
@@ -6,7 +6,7 @@ var setupButton = (myParent,url,classIdentifier) => {
     console.log(myButton)
 
     myButton.addEventListener('click', () => {
-      window.open(url, "_blank");
+      window.open('https://'+url, "_blank");
     })
 
   }
