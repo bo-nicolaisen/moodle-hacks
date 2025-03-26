ad07fb443ceb1700162c95896d97a315e9a3c236
bo-nicolaisen <bni@techcollege.dk>
2025-03-26 09:45:54 +0100

    docs

--- moodleHelpers.js
+++ moodleHelpers.js
@@ -1,4 +1,6 @@
 
+/* button link*/
+
 var setupButton = (myParent, url, classIdentifier) => {
   console.log(myParent)
   let myButton = myParent.querySelectorAll(classIdentifier)[0];
@@ -10,3 +12,14 @@ var setupButton = (myParent, url, classIdentifier) => {
     window.open(myrealUrl, "_blank");
   })
 }
+
+/* brug i moodle, kræver at moodleHelpers.js er linket ind i html'en
+
+<p>
+<button class="btn btn-secondary RepoButton" type="button"> Template repository</button>
+  <script src="https://bo-nicolaisen.github.io/moodle-hacks/moodleHelpers.js"></script>  // kan undværes hvis det er linket i toppen
+  <script>
+    setupButton(document.currentScript.parentElement,'github.com/bo-nicolaisen/moodle-hacks/blob/main/moodleHelpers.js','.RepoButton');
+  </script>
+</p>
+*/
